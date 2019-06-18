720ff76dc9dedb7ae2c460f1cb3565c6abfb2e54
MiyachiFuta <miyachi.f.aa@m.titech.ac.jp>
2019-06-18 16:28:11 +0900

    works.c終わり

--- src/works.c
+++ src/works.c
@@ -5,6 +5,31 @@ int k;
 int A[100000];
 
 
+int can_work(int max_time) {
+    int i = 0;
+    int people_num = k, sum_time = 0;
+    while(i < n && people_num > 0) {
+	int t = sum_time + A[i];
+	if(t > max_time) {
+	    sum_time = 0;
+	    people_num -= 1;
+	}
+	else {
+	    sum_time = t;
+	    i += 1;
+	}
+    }
+    return i == n;
+}
+
+int sum(int *arr, int size) {
+    int i, s = 0;
+    for(i=0; i<size; i++) {
+	s += arr[i];
+    }
+    return s;
+}
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,6 +37,15 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  ub = sum(A, n);
+  lb = 0;
+  while(ub-lb > 1) {
+      int max_time = (ub+lb)/2;
+      if(can_work(max_time)) ub = max_time;
+      else lb = max_time;
+  }
 
+  printf("%d\n", ub);
+      
   return 0;
 }
