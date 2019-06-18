1012aac8e24656dc8a05321703357ba5d6c93be8
MiyachiFuta <miyachi.f.aa@m.titech.ac.jp>
2019-06-18 15:59:32 +0900

    spear.c終わり

--- src/spear.c
+++ src/spear.c
@@ -5,6 +5,15 @@ int k;
 int A[100000];
 
 
+int can_make(int length) {
+    int i;
+    int total = 0;
+    for(i=0; i<n; i++) {
+	total += A[i]/length;
+    }
+    return total >= k;
+}
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
@@ -12,6 +21,15 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  ub = A[n-1];
+  lb = 0;
+  while(ub-lb > 1) {
+      int length = (ub + lb)/2;
+      if(can_make(length)) lb = length;
+      else ub = length;
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
